d5e870fa35f53a25b56c7a90136475d8948fd630
Simon Lefort <simonlefort@SLE.local>
2016-01-24 14:40:54 +0100

    Add vim-tmux-navigator to install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ curl -LSso ~/.custom_vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 ## Clone all the necessary plugins 
 cd ~/.custom_vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git
