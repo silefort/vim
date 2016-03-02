f9982bfa0cf23ec31a332e3afc36201c64ac8f80
slefort <simon.lefort@gmail.com>
2016-03-02 17:25:47 +0100

    Add Tabular

--- install.sh
+++ install.sh
@@ -13,3 +13,5 @@ git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/christoomey/vim-tmux-navigator.git
 git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/plasticboy/vim-markdown.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/godlygeek/tabular.git
